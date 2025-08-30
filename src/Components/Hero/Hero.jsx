a9e7cd594fee9f021731204d6e8a7d76494ea263
nazimmestar <nazim.mestar.11@gmail.com>
2025-08-30 02:00:10 +0100

    typing animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/me.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import github from '../../assets/github.svg'
-
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/me.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import github from "../../assets/github.svg";
+import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
-        <img src={profile_img} class="circle-svg" alt="" />
-        <h1><span>I'm Nazim Mestar,</span> frontend developper based in ALG</h1>
-        <p>I am a passionate web developer who loves building modern, responsive, and user-friendly applications.</p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href={'#contact'}>Connect with me</AnchorLink></div>
-            <a href="https://github.com/nazimmestar" target='_blanck'><div className="hero-resume"><img src={github} alt="" /></div></a>
+    <div id="home" className="hero">
+      <img src={profile_img} class="circle-svg" alt="" />
+      <h1>
+        <span className="heroname">I'm Nazim Mestar,</span><br />
+        <TypeAnimation
+          sequence={[
+            "Frontend Developer",
+            1000,
+            "Backend Developer",
+            1000,
+            "Software Engineering",
+            1000,
+          ]}
+          speed={50}
+          repeat={Infinity}
+          wrapper="span"
+          className="typing"
+        />
+        
+      </h1>
+      <p>
+        I am a passionate web developer based in Algeria, specializing in building modern, responsive, and intuitive applications.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href={"#contact"}>
+            Connect with me
+          </AnchorLink>
         </div>
-
+        <a href="https://github.com/nazimmestar" target="_blanck">
+          <div className="hero-resume">
+            <img src={github} alt="" />
+          </div>
+        </a>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
